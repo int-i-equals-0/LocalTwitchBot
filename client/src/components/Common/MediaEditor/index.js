923e393f531d0d73d9e8dd63506a9911881713ed
int i = 0 <emiel.vandervelde1901@gmail.com>
2026-07-21 10:46:58 +0500

    Delete client directory

--- client/src/components/Common/MediaEditor/index.js
+++
@@ -1,3 +0,0 @@
-// client/src/components/Common/MediaEditor/index.js
-
-export { default } from './MediaEditor';
--- client/src/components/Notification/NotificationConstants.js
+++
@@ -1,9 +0,0 @@
-// client/src/components/Notification/NotificationConstants.js
-
-export const NOTIFICATION_TYPES = {
-  SUCCESS: 'success',
-  ERROR: 'error',
-  WARNING: 'warning',
-  INFO: 'info',
-  CONFIRM: 'confirm'
-};
