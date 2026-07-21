a17b53231cc14d954794ca0a4b799969d140e196
int i = 0 <emiel.vandervelde1901@gmail.com>
2026-07-21 12:37:46 +0500

    Delete client directory

--- client/src/components/Notification/index.js
+++
@@ -1,5 +0,0 @@
-// client/src/components/Notification/index.js
-
-export { NotificationProvider } from './NotificationProvider';
-export { NOTIFICATION_TYPES } from './NotificationConstants';
-export { useNotification } from './NotificationContext';
